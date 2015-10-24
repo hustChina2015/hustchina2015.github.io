a90cada2196fb1471719cc8555897f2eb74028e3
hustChina2015 <hustChina2015@gmail.com>
2015-10-24 14:55:22 +0800

    maybe
    maybe

---
+++ js/load.js
@@ -0,0 +1,8 @@
+function initPage()   
+{   
+    var objLoading = document.getElementById("loader-wrapper");   
+    if (objLoading != null)   
+    {   
+        objLoading.style.display = "none";   
+    }   
+} 
